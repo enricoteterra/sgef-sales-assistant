c3078524219d1f4517acf6463b295ef9bf7445e6
Enrico Teterra (TW) <enrico.teterra@thoughtworks.com>
2019-03-16 08:50:25 +0100

    added css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import * as React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import * as styles from "./App.css";
+
+const NavBar: React.FunctionComponent = (): JSX.Element => {
+    return (
+        <div>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="title" color="inherit">
+                        React & Material-UI Sample Application
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+};
 
 class App extends React.Component {
     // eslint-disable-next-line @typescript-eslint/explicit-member-accessibility
     render() {
         return (
-            <div>
+            <div className={styles.App}>
+                <NavBar />
                 <h1>Welcome to React with Typescript</h1>
             </div>
         );
--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,18 @@ module.exports = {
                     },
                 },
             },
+            {
+                test: /\.css$/,
+                loader: "style-loader",
+            },
+            {
+                test: /\.css$/,
+                loader: "css-loader",
+                query: {
+                    modules: true,
+                    localIdentName: "[name]__[local]___[hash:base64:5]",
+                },
+            },
         ],
     },
     output: {
